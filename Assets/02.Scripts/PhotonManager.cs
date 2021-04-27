e7a8c0a989f9f17fdfafa8e7f59945af1838b19b
Jumi Oh <ojui385@gmail.com>
2021-04-27 10:41:28 +0900

    방 입장 완료

---
+++ Assets/02.Scripts/PhotonManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    private readonly string gameVersion = "v1.0";
+    private string UserId = "Ojui";
+
+    private void Awake()
+    {
+        // 게임 버전 지정
+        PhotonNetwork.GameVersion = gameVersion;
+        // 유저명 지정
+        PhotonNetwork.NickName = UserId;
+
+        // 서버 접속
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 포톤 서버에 접속했다는 것을 의미
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon Server!!!");
+        PhotonNetwork.JoinRandomRoom(); // 랜덤한 룸에 접속 시도
+    }
+
+    // 랜덤 룸에 접속 시도했으나, 만들어진 룸이 없어서 실패
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"code={returnCode}, msg={message}");
+        // 룸을 생성 > 자동 입장됨
+        PhotonNetwork.CreateRoom("My Room");
+    }
+
+    // 룸 생성 완료 콜백
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성 완료");
+    }
+
+    // 룸에 입장했을 때 호출되는 콜백함수
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 입장 완료");
+        Debug.Log(PhotonNetwork.CurrentRoom.Name);
+    }
+    
+
+
+}
