b6384b969af9e9faa22b1f866a6827b283d2ecbf
Jumi Oh <ojui385@gmail.com>
2021-04-28 09:57:11 +0900

    방 빠져나가서 로비로 가기

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 using Photon.Pun;
 using TMPro;
 using UnityEngine.UI;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public TMP_Text roomNameText;
+    public TMP_Text connectInfoText;
+    public Button exitButton;
+    
     private void Awake()
     {
         Vector3 pos = new Vector3(Random.Range(-200.0f, 200.0f), 5.0f, Random.Range(-200.0f, 200.0f));
@@ -14,4 +20,32 @@ public class GameManager : MonoBehaviourPunCallbacks
         // 통신이 가능한 주인공 캐릭터(탱크) 생성
         PhotonNetwork.Instantiate("Tank", pos, Quaternion.identity, 0);
     }
+    
+    void Start()
+    {
+        SetRoomInfo();
+    }
+
+    void SetRoomInfo()
+    {
+        Room currentRoom =  PhotonNetwork.CurrentRoom;
+        roomNameText.text = currentRoom.Name;
+        connectInfoText.text = $"{currentRoom.PlayerCount}/{currentRoom.MaxPlayers}";
+    }
+
+    // 클론을 지우는 등 cleanUp 작업
+    public void OnExitClick()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    // CleanUp 끝난 후에 호출되는 콜백
+    // 이미 로비에 있는 상태이므로 씬만 바꿔주면 됨
+    public override void OnLeftRoom()
+    {
+        // Lobby 씬으로 되돌아 가기...
+        SceneManager.LoadScene("Lobby");
+    }
+
+
 }
